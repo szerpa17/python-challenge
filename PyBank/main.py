3db652ffe9c5b583c77f556e715db70b2469a34c
szerp <szerp002@gmail.com>
2020-06-17 01:10:45 -0400

    PyBank/main.py file updates: Updated total_months function. Created average_change function. Included print statements to verify results.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,26 @@ import csv
 
 # Create functions for calculations
 # Total months - calculated by creating a list and then finding the length of the list
-def total_months(months_list):
+def total_months(month_list):
     months = len(month_list)
-    return months_list, len(months)
+    return months
 
 # Net profit/loss - calculated by converting the first column of every row
 def net_total(profit_list):
     total = sum(profit_list)
     return total
+
+def average_change(profit_list):
+    change = []
+    for index, profit in enumerate(profit_list):
+        if (index == len(profit_list)-1):
+            break
+        else:
+            change_val = (profit_list[index+1]-profit_list[index])
+            change.append(change_val)            
+    average_change = sum(change)/len(change)
+    return average_change
+        
 
 # Insert location of csv file to be used for analysis in the file_path variable
 file_path = os.path.join('Resources', 'budget_data.csv')
@@ -37,10 +49,11 @@ with open (file_path, 'r') as csvfile:
         months.append(row[0][0:3])
         profit = int(row[1])
         total.append(profit)
-        # print(months, total)
-        # print(total_months(months))
-        # print(net_total(total))
         
         # Created this conditional to test functions and analyis on the first 3 rows
         if row[0] == 'Mar-2010':
             break
+    
+    print(f"Total Months: {total_months(months)}")
+    print(f"Total: {net_total(total)}")
+    print(f"Average Change: {average_change(total)}")
