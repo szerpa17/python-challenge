f8f3fbdd4a3790037bbc9c4c3ea72130ab417bd2
szerp <szerp002@gmail.com>
2020-06-17 00:19:00 -0400

    PyBank/main.py file updates: Created (months, and total) list variables that are external from the functions used. Updated the total_months function so that it will accept a list. Created the net_total function, which also accepts a list, to tally the company’s net profit/loss for all rows. Appended a conditional in order to later check functions against the first three data entries on the list.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,14 @@ import csv
 
 # Create functions for calculations
 # Total months - calculated by creating a list and then finding the length of the list
-def total_months(row):
-    month = str(row[0])
-    month = month[:3]
-    months = []
-    months.append(month)
-    return months, len(months)
+def total_months(months_list):
+    months = len(month_list)
+    return months_list, len(months)
+
+# Net profit/loss - calculated by converting the first column of every row
+def net_total(profit_list):
+    total = sum(profit_list)
+    return total
 
 # Insert location of csv file to be used for analysis in the file_path variable
 file_path = os.path.join('Resources', 'budget_data.csv')
@@ -26,9 +28,19 @@ with open (file_path, 'r') as csvfile:
     # print(csvreader, type(csvreader))
     print(csvheader, type(csvheader))
 
+    months = []
+    total = []
+
     # Go through every line of the csv
     for row in csvreader:
         #print(row)
-        print(total_months(row))
-        # Break inserted to test code on one row
-        break
+        months.append(row[0][0:3])
+        profit = int(row[1])
+        total.append(profit)
+        # print(months, total)
+        # print(total_months(months))
+        # print(net_total(total))
+        
+        # Created this conditional to test functions and analyis on the first 3 rows
+        if row[0] == 'Mar-2010':
+            break
