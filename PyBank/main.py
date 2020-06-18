e75513196173c01f83eac259a61750c8d0efe14e
szerp <szerp002@gmail.com>
2020-06-17 23:06:26 -0400

    PyBank/main.py file updates: Updated the string returned by the financial_analysis function to return the month and year of the greatest increase and decrease in profits. Reorganized the code/comments in the function. Removed the slicer in the csv reader's for loop in order to transfer both the month and the year data from row[0] to the new list.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,29 +12,34 @@ def financial_analysis (month_list, profit_list):
     # This function excludes the last item on the list (as there is no 
     # index + 1 to calculate with).
     change = []
+    g_increase = 0
+    g_decrease = 0
+    inc_month = ""
+    dec_month = ""
     for index, profit in enumerate(profit_list):
         if (index == len(profit_list)-1):
             break
         else:
             change_val = (profit_list[index+1]-profit_list[index])
-            change.append(change_val)            
-    average_change = sum(change)/len(change)    
+            change.append(change_val)    
+        average_change = sum(change)/len(change)        
     # Greatest increase/decrease - calculated via the use of a conditional
-    # in order to assign new values to variables.
-    g_increase = 0
-    g_decrease = 0
-    for index, profit in enumerate(profit_list):
         if profit > g_increase:
             g_increase = profit
-        elif profit < g_decrease:
+            inc_month = month_list[index]
+        if profit < g_decrease:
             g_decrease = profit
+            dec_month = month_list[index]
+    # Reference for currency format: https://stackoverflow.com/questions/320929/currency-formatting-in-python  
     results = ("Financial Analysis" +
-                "\n----------------------------"
+                "\n----------------------------" +
                 "\nTotal Months: " + str(months) +
-                " \nTotal: " + str(total) + 
-                " \nAverage Change: " + str(average_change) +
-                " \nGreatest Increase in Profits: " + str(g_increase) +
-                " \nGreatest Decrease in Profits: " + str(g_decrease))
+                "\nTotal: " + str('${:,.2f}'.format(total)) + 
+                "\nAverage Change: " + str('${:,.2f}'.format(average_change)) +
+                "\nGreatest Increase in Profits: " + inc_month + " " +
+                str('${:,.2f}'.format(g_increase)) +
+                "\nGreatest Decrease in Profits: " + dec_month + " " +
+                str('${:,.2f}'.format(g_decrease)))
     return results
     
 
@@ -60,7 +65,8 @@ with open (file_path, 'r') as csvfile:
     # Go through every line of the csv
     for row in csvreader:
         # print(row)
-        months.append(row[0][0:3])
+        # Removed [0:3] slicer in the month list
+        months.append(row[0]) 
         profit = int(row[1])
         total.append(profit)
         
